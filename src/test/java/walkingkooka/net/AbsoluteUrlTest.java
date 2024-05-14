dafa84c01859b827379427d90fe08ad945030820
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-14 23:01:35 +1000

    UrlFragment.toString unnecessary encoding of some chars FIX
    - Unfortunately java's URLEncoder isnt a perfect match for fragment encodings.
    
    - Closes https://github.com/mP1/walkingkooka-net/issues/479
    - UrlFragment.toString shouldnt be escaping equals-sign

--- src/test/java/walkingkooka/net/AbsoluteUrlTest.java
+++ src/test/java/walkingkooka/net/AbsoluteUrlTest.java
@@ -691,7 +691,7 @@ public final class AbsoluteUrlTest extends AbsoluteOrRelativeUrlTestCase<Absolut
         this.checkPortAbsent(url);
         this.checkPath(url, UrlPath.parse("/path123"));
         this.checkQueryString(url, UrlQueryString.parse("query456"));
-        this.checkFragment(url, UrlFragment.with("fragment "));
+        this.checkFragment(url, UrlFragment.with("fragment+"));
 
         this.toStringAndCheck(
                 url,
@@ -840,7 +840,7 @@ public final class AbsoluteUrlTest extends AbsoluteOrRelativeUrlTestCase<Absolut
     public void testToStringFragmentPercent20() {
         this.toStringAndCheck(
                 AbsoluteUrl.parseAbsolute("http://example/path123?query456#fragment%20"),
-                "http://example/path123?query456#fragment+"
+                "http://example/path123?query456#fragment "
         );
     }
 
