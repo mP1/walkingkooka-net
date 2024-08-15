ac743e15b46bfe673ebd3dbe3e7973ab8ae9c8b7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-15 18:24:42 +1000

    HeaderParameterName javadoc

--- src/main/java/walkingkooka/net/header/HeaderParameterName.java
+++ src/main/java/walkingkooka/net/header/HeaderParameterName.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 /**
- * Base class for any {@link HeaderName}.
+ * Base class for any parameter belonging to a header.
  */
 abstract class HeaderParameterName<V> extends HeaderName2<V> {
 
