c1d3528df0ead33875cd44ad5583f56be4bf4157
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-09-30 21:27:12 +1000

    NetConverterTextToHostAddress

---
+++ src/test/java/walkingkooka/net/convert/NetConverterTextToHostAddressTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.net.convert;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.Either;
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.ConverterTesting2;
+import walkingkooka.convert.Converters;
+import walkingkooka.convert.FakeConverterContext;
+import walkingkooka.net.HostAddress;
+
+public final class NetConverterTextToHostAddressTest implements ConverterTesting2<NetConverterTextToHostAddress<FakeConverterContext>, FakeConverterContext> {
+
+    @Test
+    public void testConvertCharSequenceToHostAddress() {
+        final String hostAddress = "example.com";
+
+        this.convertAndCheck(
+            new StringBuilder(hostAddress),
+            HostAddress.with(hostAddress)
+        );
+    }
+
+    @Test
+    public void testConvertStringToHostAddress() {
+        final String hostAddress = "example.com";
+
+        this.convertAndCheck(
+            hostAddress,
+            HostAddress.with(hostAddress)
+        );
+    }
+
+    @Test
+    public void testConvertStringToUrlFails() {
+        this.convertFails(
+            "https://example.com",
+            HostAddress.class
+        );
+    }
+
+    @Override
+    public NetConverterTextToHostAddress<FakeConverterContext> createConverter() {
+        return NetConverterTextToHostAddress.instance();
+    }
+
+    @Override
+    public FakeConverterContext createContext() {
+        return new FakeConverterContext() {
+
+            @Override
+            public boolean canConvert(final Object value,
+                                      final Class<?> type) {
+                return this.converter.canConvert(
+                    value,
+                    type,
+                    this
+                );
+            }
+
+            @Override
+            public <T> Either<T, String> convert(final Object value,
+                                                 final Class<T> target) {
+                return converter.convert(
+                    value,
+                    target,
+                    this
+                );
+            }
+
+            private final Converter<FakeConverterContext> converter = Converters.characterOrCharSequenceOrHasTextOrStringToCharacterOrCharSequenceOrString();
+        };
+    }
+
+    @Override
+    public Class<NetConverterTextToHostAddress<FakeConverterContext>> type() {
+        return Cast.to(NetConverterTextToHostAddress.class);
+    }
+
+    @Override
+    public String typeNamePrefix() {
+        return "NetConverter";
+    }
+
+    @Override
+    public String typeNameSuffix() {
+        return "";
+    }
+}
