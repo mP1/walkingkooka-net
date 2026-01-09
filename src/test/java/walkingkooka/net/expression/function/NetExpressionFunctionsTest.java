808f95b4cf0bba85bc28c374b62cfbaa3a9fb45e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-01-09 18:47:11 +1100

    https://github.com/mP1/walkingkooka-convert/pull/401 ConverterContext extends HasLineEnding
    - https://github.com/mP1/walkingkooka-convert/pull/401
    - ConverterContext extends HasLineEnding

--- src/test/java/walkingkooka/net/expression/function/NetExpressionFunctionsTest.java
+++ src/test/java/walkingkooka/net/expression/function/NetExpressionFunctionsTest.java
@@ -36,6 +36,7 @@ import walkingkooka.net.email.EmailAddress;
 import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 import walkingkooka.text.CaseSensitivity;
+import walkingkooka.text.LineEnding;
 import walkingkooka.tree.expression.Expression;
 import walkingkooka.tree.expression.ExpressionEvaluationContexts;
 import walkingkooka.tree.expression.ExpressionFunctionName;
@@ -404,6 +405,7 @@ public final class NetExpressionFunctionsTest implements PublicStaticHelperTesti
                     ConverterContexts.basic(
                         false, // canNumbersHaveGroupSeparator
                         Converters.EXCEL_1900_DATE_SYSTEM_OFFSET, // dateTimeOffset
+                        LineEnding.NL,
                         ',', // valueSeparator
                         NetConverters.net(),
                         DateTimeContexts.fake(),
