739531e67ed0efce8f684a85d238ec7092606b1c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-06-30 17:45:09 +1000

    GetOrHeadHttpHandler was GetOrHeadHeaderHateosHttpHandler

---
+++ src/test/java/walkingkooka/net/http/server/GetOrHeadHttpHandlerTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.net.http.server;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.net.Url;
+import walkingkooka.net.header.HttpHeaderName;
+import walkingkooka.net.http.HttpEntity;
+import walkingkooka.net.http.HttpMethod;
+import walkingkooka.net.http.HttpProtocolVersion;
+import walkingkooka.net.http.HttpStatusCode;
+import walkingkooka.net.http.HttpTransport;
+import walkingkooka.reflect.JavaVisibility;
+
+public final class GetOrHeadHttpHandlerTest implements GetOrHeadHttpHandlerTesting<GetOrHeadHttpHandler<FakeHttpHandlerContext>, FakeHttpHandlerContext> {
+
+    @Test
+    public void testHandlePostMethodNotAllowed() {
+        final HttpResponse response = HttpResponses.recording();
+        response.setStatus(HttpStatusCode.METHOD_NOT_ALLOWED.setMessage("Method POST not allowed"));
+        response.setEntity(
+            HttpEntity.EMPTY.addHeader(
+                HttpHeaderName.ALLOW,
+                Lists.of(
+                    HttpMethod.GET,
+                    HttpMethod.HEAD
+                )
+            )
+        );
+
+        this.handleAndCheck(
+            HttpRequests.post(
+                HttpTransport.UNSECURED,
+                Url.parseRelative("/api/decimalNumberSymbols/*/localeStartsWith/English?offset=0&count=2"),
+                HttpProtocolVersion.VERSION_1_0,
+                HttpEntity.EMPTY.addHeader(
+                    HttpHeaderName.ALLOW,
+                    Lists.of(
+                        HttpMethod.HEAD,
+                        HttpMethod.GET
+                    )
+                )
+            ),
+            this.createContext(),
+            response
+        );
+    }
+
+
+    @Override
+    public GetOrHeadHttpHandler<FakeHttpHandlerContext> createHttpHandler() {
+        return new GetOrHeadHttpHandler<>() {
+
+            @Override
+            public void handleGetOrHead(final HttpRequest request,
+                                        final HttpResponse response,
+                                        final FakeHttpHandlerContext context) {
+                response.setStatus(
+                    HttpStatusCode.OK.setMessage("Hello")
+                );
+                response.setEntity(
+                    HttpEntity.EMPTY.setBodyText("World")
+                );
+            }
+        };
+    }
+
+    @Override
+    public FakeHttpHandlerContext createContext() {
+        return new FakeHttpHandlerContext();
+    }
+
+    @Override
+    public Class<GetOrHeadHttpHandler<FakeHttpHandlerContext>> type() {
+        return Cast.to(GetOrHeadHttpHandler.class);
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
