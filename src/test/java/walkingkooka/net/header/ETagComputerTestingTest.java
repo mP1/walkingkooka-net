7a28358bd28a9363b88825ff48ca8000f7aa3322
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-25 13:51:04 +1000

    ETagComputerTesting

---
+++ src/test/java/walkingkooka/net/header/ETagComputerTestingTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.net.header;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Binary;
+
+import java.util.Optional;
+
+public final class ETagComputerTestingTest implements ETagComputerTesting {
+
+    @Test
+    public void testComputeETagEmpty() {
+        this.computeETagAndCheck(
+            new ETagComputer() {
+                @Override
+                public Optional<ETag> computeEtag(final Binary binary) {
+                    return Optional.empty();
+                }
+            },
+            Binary.EMPTY
+        );
+    }
+
+    @Test
+    public void testComputeETagSuccess() {
+        final ETag etag = ETag.with(
+            "123",
+            ETagValidator.STRONG
+        );
+
+        this.computeETagAndCheck(
+            new ETagComputer() {
+                @Override
+                public Optional<ETag> computeEtag(final Binary binary) {
+                    return Optional.of(etag);
+                }
+            },
+            Binary.EMPTY,
+            etag
+        );
+    }
+}
